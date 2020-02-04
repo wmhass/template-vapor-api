bd5e2cda0512166c9e0284bc3bd6dfa2f483543d
William Hass <wm.hass@gmail.com>
2020-02-04 22:22:38 +0100

    added tests for fail user creation

--- Sources/App/Endpoints/User/UseCases/Tests/CreateUserUseCaseTests.swift
+++ Sources/App/Endpoints/User/UseCases/Tests/CreateUserUseCaseTests.swift
@@ -1,6 +1,7 @@
 @testable import App
 import Vapor
 import XCTest
+import PostgreSQL
 
 final class CreateUserUseCaseTests: AppTesCase {
 
@@ -15,5 +16,28 @@ final class CreateUserUseCaseTests: AppTesCase {
         let createUserUseCase = CreateUserUseCase(user: createUserRequest, db: try dbConnection())
         let newUser = try createUserUseCase.createUser().wait()
         XCTAssertNotNil(newUser.id)
+    }
+    
+    func testUserCreationFailed() throws {
+        try deleteAllUsers()
+        let createUserRequest = App.CreateUserRequestContent(
+            name: "Will Hass",
+            email: "mail@mail.com",
+            password: "1234",
+            verifyPassword: "1234"
+        )
+        let createUserUseCase = CreateUserUseCase(user: createUserRequest, db: try dbConnection())
+        let newUser = try createUserUseCase.createUser().wait()
+        do {
+            XCTAssertNotNil(newUser.id)
+            let result = try createUserUseCase.createUser().wait()
+            XCTFail("Failed: Shouldn't have createad user \(result.email)")
+        } catch _ as PostgreSQLError {
+            XCTAssertTrue(true, "Success: User already exists")
+        } catch let exception {
+            XCTFail("Failed: \(exception.localizedDescription)")
+        }
+
+        
     }
 }
